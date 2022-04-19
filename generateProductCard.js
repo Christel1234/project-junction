11b3d6de9c36a9b0f53ce960513dfbb17c4819b1
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-04-19 03:33:33 +0200

    Add localStorage cart functionality setup

--- generateProductCard.js
+++ generateProductCard.js
@@ -1,4 +1,5 @@
 import { calculateDiscount } from "./utils.js";
+import { addToCart } from "./shoppingCart.js";
 
 /**
  * Generates product card for single product
---
+++ shoppingCart.js
@@ -0,0 +1,11 @@
+function addToCart(product) {
+  //
+}
+
+function removeFromCart() {}
+function getCartTotal() {}
+function clearCart() {
+  localStorage.clear();
+}
+
+export { addToCart, removeFromCart, getCartTotal, clearCart };
